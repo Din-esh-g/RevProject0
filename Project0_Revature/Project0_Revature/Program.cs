cb4773118e00afe46eef7bfa92b80380edb38f00
Dinesh Ghimire <dineshg822@gmail.com>
2019-10-19 11:43:59 -0400

    Load Base Project

---
+++ Project0_Revature/Project0_Revature/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project0_Revature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
